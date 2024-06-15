0aafa4b9c263a5919bdf94d2bb2514887ea6bd56
goergemiguel <goergemiguel21@gmail.com>
2024-06-15 15:58:32 +0800

    feat: update light theme

--- src/components/chat/ChatBubble.tsx
+++ src/components/chat/ChatBubble.tsx
@@ -22,7 +22,7 @@ export default component$((props: ChatBubbleProps) => {
 		<div id="chat_bubble" class={`flex flex-col mb-4 ${props.message.role == 'user' ? 'items-end' : 'items-start'}`} >
 			<p
 				class={`p-8 mb-1 w-2/3 text-sm ${props.message.role == 'user' ?
-					'bg-primary dark:bg-gray-800 text-white rounded-tl-xl rounded-tr-xl rounded-bl-xl'
+					'bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white rounded-tl-xl rounded-tr-xl rounded-bl-xl'
 					: 'bg-white dark:bg-gray-400 border border-gray-200 dark:border-gray-400 text-gray-800 rounded-tr-xl rounded-br-xl rounded-tl-xl '}`}
 			>
 				{props?.loading ? <LoadingIcon class="w-6 h-6 animate-spin" /> : <div dangerouslySetInnerHTML={formattedContent.value} />}
